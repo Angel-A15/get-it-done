a129620aa1df6dbd9f113b29765023065393de3e
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-08-18 02:55:57 -0500

    added more suiting messages users will find appealing

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,4 @@
+var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
 
 var getReposIssues=function(repo){
@@ -15,6 +16,17 @@ var getReposIssues=function(repo){
             alert("There was a problem with your request!");
         }
     });
+
+    if(response.ok) {
+        response.json().then(function(data){
+            dislpayIssues(data);
+
+            //check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
+        });
+    }
     
 
 };
@@ -61,6 +73,20 @@ var dislpayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 
+};
+
+var displayWarning = function(repo){
+    //add text to warning container/update text
+    limitWarningEl.textContent="To see more than 30 issues, visit";
+
+    //append a link element with an href attribute that points to url site 
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEL.setAttr("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
 
 getReposIssues("facebook/react");
